a762fc1ee213269157aea102c947366dbf1df200
Guest User <Guest@stk-cs01-ws06.westlothian.sch.uk>
2015-12-16 12:28:14 +0000

    2.6

--- __SHELL44.java
+++ __SHELL44.java
@@ -1,5 +1,5 @@
 
-public class __SHELL40 extends bluej.runtime.Shell {
+public class __SHELL44 extends bluej.runtime.Shell {
 public static void run() throws Throwable {
 final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/Guest/Documents/Essell-Academy");
 final SHOW sHOW1 = (SHOW)__bluej_runtime_scope.get("sHOW1");
